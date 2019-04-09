244fa5bf074889d56c42f6afedeeef2efe6a7d12
Strannyk <strannyk8@gmail.com>
2019-04-09 20:46:13 +0300

    Navbar adding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/admin">Admin</router-link>
-    </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+          <span class="navbar-brand main-link" ><router-link to="/">Rapuha</router-link></span>
+      </nav>
     <router-view/>
   </div>
 </template>
