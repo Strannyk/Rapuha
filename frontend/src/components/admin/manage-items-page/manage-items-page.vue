78f669d05a3a73bd690228fc53087121c33556d1
Strannyk <strannyk8@gmail.com>
2019-05-22 21:43:47 +0300

    Admin panel update

--- frontend/src/components/admin/manage-items-page/manage-items-page.vue
+++ frontend/src/components/admin/manage-items-page/manage-items-page.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="r-block r-center">
+        <h2>Панель администратора</h2>
         <div class="btn-group btn-group-toggle" data-toggle="buttons">
-            <router-link to="/admin/quotes">
-                <button type="button" class="btn btn-primary">Цитаты</button>
-            </router-link>
-            <router-link to="/admin/reflections">
-                <button type="button" class="btn btn-primary">Размышления</button>
-            </router-link>
-            <router-link to="/admin/stories">
-                <button type="button" class="btn btn-primary">Рассказы</button>
-            </router-link>
+            <div>
+                <router-link to="/admin/reflections">
+                    <button type="button" class="btn btn-primary btn-lg btn-block r-button">Размышления</button>
+                </router-link>
+                <router-link to="/admin/quotes">
+                    <button type="button" class="btn btn-primary btn-lg btn-block r-button">Цитаты</button>
+                </router-link>
+            </div>
+            <div>
+                <router-link to="/admin/stories">
+                    <button type="button" class="btn btn-primary btn-lg btn-block r-button">Рассказы</button>
+                </router-link>
+                <router-link to="/admin/feedback">
+                    <button type="button" class="btn btn-primary btn-lg btn-block r-button">Обратная связь</button>
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
 
-<script src="./manage-items-page.js"></script>
+<style scoped src="./manage-items-page.scss" lang="scss"></style>
