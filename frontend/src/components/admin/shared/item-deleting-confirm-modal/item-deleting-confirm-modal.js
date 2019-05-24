0c5f7f8ce49b1e13d3d59ca001665bb013a1bbca
Mikhail Pikulik <strannyk8@gmail.com>
2019-05-24 19:17:11 +0300

    Code refactoring (inception)

--- frontend/src/components/admin/shared/item-deleting-confirm-modal/item-deleting-confirm-modal.js
+++ frontend/src/components/admin/shared/item-deleting-confirm-modal/item-deleting-confirm-modal.js
@@ -4,47 +4,8 @@ export default {
   mixins: [modalMixin],
 
   props: {
-    postType: String
-  },
-
-  computed: {
-    titleWording: function () {
-      const action = 'Удаление ';
-
-      switch (this.postType) {
-        case 'reflection': {
-          return action + 'размышления';
-        }
-        case 'story': {
-          return action + 'рассказа';
-        }
-        case 'quote': {
-          return action + 'цитаты';
-        }
-        case 'feedback': {
-          return action + ' обратной связи';
-        }
-      }
-    },
-
-    bodyWording: function () {
-      const action = 'Удалить ';
-
-      switch (this.postType) {
-        case 'reflection': {
-          return action + 'размышление?';
-        }
-        case 'story': {
-          return action + 'рассказ?';
-        }
-        case 'quote': {
-          return action + 'цитату?';
-        }
-        case 'feedback': {
-          return action + ' обратную связь?';
-        }
-      }
-    }
+    titleWording: String,
+    bodyWording: String
   },
 
   methods: {
